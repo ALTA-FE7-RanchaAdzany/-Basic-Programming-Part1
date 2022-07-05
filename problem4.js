ee9bf8f6da513d949d0c9ca0a49b8255d096ead8
ray34-cyber <rancha.adzany1@gmail.com>
2022-07-05 13:38:21 +0800

    tugas selesai

---
+++ problem4.js
@@ -0,0 +1,4 @@
+let alas = 20;
+let tinggi = 25;
+let luasSegitiga = (alas * tinggi) / 2;
+console.log(luasSegitiga);
---
+++ problem5.js
@@ -0,0 +1,7 @@
+const pi = 3.14 || 22 / 7;
+let r = 4;
+let T = 20;
+
+let Lp = 2 * pi * r * (r + T);
+
+console.log(Lp);
